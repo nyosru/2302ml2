1542160bfa8c59e2d22af3360dec665e1c37f508
Сергей <1@php-cat.com>
2023-02-10 03:37:44 +0500

    11

--- src/sd_scanGoods-dev.js
+++ src/sd_scanGoods-dev.js
@@ -1,6 +1,6 @@
 // require('./sd2.js')
 
-import axios from 'axios';
+// import axios from 'axios';
 // const { aaa, aaa2 } = require('./inin')
 
 const {
@@ -148,122 +148,6 @@ async function start() {
                 // await loadPage(telega, 'https://api.uralweb.info/telegram.php?s=1&token=6272013314:AAE87uoGgRkLaKnFMuW2zkUqlAeJ_e9YyUg&domain=parser.php-cat.com&msg=ok ' + good.name)
                 telegas('ok ' + good.name)
 
-                continue
-
-                // скан каталогов
-                let catParce = await SD_GetCatForScan(connection)
-
-                if (catParce === undefined) {
-                    console.error('НЕТ НИЧЕГО')
-                    continue
-                }
-                // console.log('catParce', catParce)
-
-                let catUri = catParce.l0
-                let catName = catParce.n0
-
-                console.log('')
-                console.log('парсим каталог ', catUri)
-                    // console.log('ii', i);
-                if (i == 1) {
-                    console.log('загрузка страницы')
-                    await loadPage(page, 'https://www.sdvor.com/tmn/category/' + catUri)
-                    await page.sleep(500)
-                    await clickCloseUpModal(page)
-                        // await page.sleep(2 * 1000)
-                        // await clickCloseUpModal(page)
-                } else {
-                    console.log('переход по меню ')
-                        // await SDGoToCatNav(page, catParce, catUri)
-                        // await clickCloseUpModal(page)
-                    await SDGoToCatNav(page, catParce, catUri)
-                        // console.log('gogo ', 'https://www.sdvor.com/tmn/category/' + catUri)
-                        // await SDGoToCatNavFast(page, 'https://www.sdvor.com/tmn/category/' + catUri)
-                }
-
-                await page.sleep(1 * 1000)
-                await creatScreenshot(page)
-                await page.sleep(5 * 1000)
-
-                check = await checkNowCat(page, catName)
-
-                console.log('')
-                console.log(catName)
-
-                if (check) {
-                    console.error('++ загруженный каталог соответствует нужному')
-                    console.error('++ загруженный каталог соответствует нужному')
-                    console.error('++ загруженный каталог соответствует нужному')
-                } else {
-                    console.error('-- загруженный каталог не соответствует нужному')
-                    console.error('-- загруженный каталог не соответствует нужному')
-                    console.error('-- загруженный каталог не соответствует нужному')
-                    console.error('идём на след заход')
-                    continue
-                }
-
-                await pageScrollToBottom(page, 3)
-                await creatScreenshot(page)
-
-                goods = await loadGoodsFromListCat(page, priceNormalizator)
-                    // console.log('goods', goods)
-
-                for (let good of goods) {
-                    // console.log('good', good)
-                    console.log('   good / ', good.name)
-                    good.cat = catUri
-
-                    let res = await saveToDb(
-                        connection,
-                        'sd_good', [
-                            'name',
-                            'uri',
-                            // 'kod_str',
-                            'kod',
-                            'img',
-                            'cat',
-                            'price_str',
-                            'price',
-                            'price_dop',
-                            'created_at',
-                        ],
-                        good,
-                    )
-                }
-
-                continue
-
-                await UpModalCityClose(page)
-                await page.sleep(3 * 1000)
-                await creatScreenshot(page, './img/11.png')
-
-                await pageScrollToBottom(page, 3)
-                await creatScreenshot(page, './img/11.png')
-
-                goods = await loadGoodsFromListCat(page, priceNormalizator)
-                console.log('goods', goods)
-
-                for (let good of goods) {
-                    console.log('good', good)
-                    good.cat = catUri
-
-                    let res = await saveToDb(
-                        connection,
-                        'sd_good', [
-                            'name',
-                            'uri',
-                            // 'kod_str',
-                            'kod',
-                            'img',
-                            'cat',
-                            'price_str',
-                            'price',
-                            'price_dop',
-                            'created_at',
-                        ],
-                        good,
-                    )
-                }
             }
         } catch (error) {
             console.error('after SD_GetCatForScan', error)
@@ -289,6 +173,6 @@ async function start() {
 }
 
 // loadPage(telega, 'https://api.uralweb.info/telegram.php?s=1&token=6272013314:AAE87uoGgRkLaKnFMuW2zkUqlAeJ_e9YyUg&domain=parser.php-cat.com&msg=01 pars: старт')
-telegas('01 pars: старт')
+// telegas('01 pars: старт')
 
 start()
